8b1092cb024a977517e4b8592b7e7abbabb77ed4
mohammedabarh <abmohammed606@gmail.com>
2025-02-17 16:11:43 -0800

    task

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Contains the TestAmenityDocs classes
+This code is a part of a software framework and is provided for educational purposes only. 
+All rights reserved to the original author.
+"""
+
+from datetime import datetime
+import inspect
+import models
+from models import amenity
+from models.base_model import BaseModel
+import pep8
+import unittest
+Amenity = amenity.Amenity
+
+
+class TestAmenityDocs(unittest.TestCase):
+    """Tests to check the documentation and style of Amenity class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.amenity_f = inspect.getmembers(Amenity, inspect.isfunction)
+
+    def test_pep8_conformance_amenity(self):
+        """Test that models/amenity.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_amenity(self):
+        """Test that tests/test_models/test_amenity.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_amenity_module_docstring(self):
+        """Test
